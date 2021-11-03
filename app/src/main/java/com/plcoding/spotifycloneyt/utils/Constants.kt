aa2650c892da4bd088b5627d10934fc8829beb0e
Hankerz <ahmedwalid98.aw@gmail.com>
2021-11-03 13:22:02 +0200

    Spotify

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.plcoding.spotifycloneyt.utils
+
+object Constants {
+    const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_ID = 1
+    const val MEDIA_ROOT_ID = "media_id"
+    const val NOTIFICATION_CHANNEL_ID = "music"
+
+    const val NETWORK_ERROR = "network_error"
+    const val UPDATE_PLAYER_POSITION_INTERVAL = 100L
+}
